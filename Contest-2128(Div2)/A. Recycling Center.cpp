6d29be65f97572a8720a4cbacc892b19d27a47c9
= <=>
2025-08-13 16:31:56 +0530

    Grind

--- Contest-2128(Div2)/A. Recycling Center.cpp
+++ Contest-2128(Div2)/A. Recycling Center.cpp
@@ -1,13 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
+
     int t;
     cin >> t;
-    while (t--) {
+    while (t--)
+    {
         int n;
         long long c;
         cin >> n >> c;
@@ -15,20 +17,26 @@ int main() {
         vector<long long> a(n);
         for (auto &x : a) cin >> x;
 
-        sort(a.begin(),a.end());
+        sort(a.begin(), a.end());
         reverse(a.begin(), a.end());
 
         int coins = 0;
         int time = 0;
-        for (int i = 0; i < n; ++i) {
-            if(a[i]>c){
+        for (int i = 0; i < n; ++i)
+        {
+            if (a[i] > c)
+            {
                 coins++;
             }
-            else{
-                int count = floor(log2(c/a[i]))+1;
-                if(time < count){
+            else
+            {
+                int count = floor(log2(c / a[i])) + 1;
+                if (time < count)
+                {
                     time++;
-                }else{
+                }
+                else
+                {
                     coins++;
                 }
             }
