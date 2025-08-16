1b1efccab56f3e540e1d0a5d5ba80cf459b04dab
shivanshcoding <shivanshcoding2005@gmail.com>
2025-08-17 01:15:27 +0530

    Grind for 17 Aug 2025

---
+++ ProblemSolved/1360A. Minimal Square.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio() ios::sync_with_stdio(false); cin.tie(nullptr)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define f first
+#define s second
+#define sz(x) (int)(x).size()
+using ll = long long;
+using ld = long double;
+using pll = pair<ll,ll>;
+template<typename T> void printv(const vector<T>& v) { for (auto &x : v) cout << x << ' '; }
+template<typename T> void print2d(const vector<vector<T>>& v) { for (auto &row : v) { for (auto &x : row) cout << x << ' '; cout << '\n'; } }
+ll t=1,n,k,a,b,q,x,y, MOD = 1e9+7;
+
+void solve() {
+    cin >> a >> b;
+    ll side = min(max(a * 2, b), max(a, b * 2));
+    cout<< side * side<<endl; 
+}
+
+int main() {
+    fastio();
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
---
+++ ProblemSolved/1367B. Even Array.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio() ios::sync_with_stdio(false); cin.tie(nullptr)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define f first
+#define s second
+#define sz(x) (int)(x).size()
+using ll = long long;
+using ld = long double;
+using pll = pair<ll,ll>;
+template<typename T> void printv(const vector<T>& v) { for (auto &x : v) cout << x << ' '; }
+template<typename T> void print2d(const vector<vector<T>>& v) { for (auto &row : v) { for (auto &x : row) cout << x << ' '; cout << '\n'; } }
+ll t=1,n,k,a,b,q,x,y, MOD = 1e9+7;
+
+void solve() {
+    cin >> n;
+    a = 0, b = 0;
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+        if (x % 2 != i % 2) {
+            if (i % 2 == 0) {
+                a++;
+            } else {
+                b++;
+            }
+        }
+    }
+    if (a != b) {
+        cout << -1 << endl;
+    } else {
+        cout << a << endl;
+    }
+}
+
+int main() {
+    fastio();
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
