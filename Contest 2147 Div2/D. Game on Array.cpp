103f61927c1ba1290e9821a5657b8ea29d0e8b1b
shivanshcoding <shivanshcoding2005@gmail.com>
2025-09-21 01:11:47 +0530

    Corrections in D

--- Contest 2147 Div2/D. Game on Array.cpp
+++ Contest 2147 Div2/D. Game on Array.cpp
@@ -1,75 +1,67 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <cstdlib>
+#include <set>
+#include <map>
 using namespace std;
-struct custom_hash {
-   static uint64_t splitmix64(uint64_t x) {
-       x += 0x9e3779b97f4a7c15;
-       x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-       x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-       return x ^ (x >> 31);
-   }
-size_t operator()(uint64_t x) const {
-       static const uint64_t FIXED_RANDOM =
-       chrono::steady_clock::now().time_since_epoch().count();
-       return splitmix64(x + FIXED_RANDOM);
-   }
-};
-#define prDouble(x) cout<<fixed<<setprecision(10)<<x
-#define fastio() ios::sync_with_stdio(false); cin.tie(nullptr)
-#define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
-#define pb push_back
-#define s second
-#define sz(x) (int)(x).size()
-using ll = long long;
-using ld = long double;
-using pll = pair<ll,ll>;
-using tll = tuple<ll,ll,ll>;
-using vll = vector<ll>;
-using vpll = vector<pll>;
-vector<ll> dx = {1, -1, 0, 0}, dy = {0, 0, 1, -1}; // for grid
-vector<ll> ddx = {1,1,0,-1,-1,-1,0,1}, ddy = {0,1,1,1,0,-1,-1,-1}; // 8 directions
-template<typename T> void read(vector<T> &v) { for (auto &x : v) cin >> x; }
-template<typename T> void printv(const vector<T>& v) { for (auto &x : v) cout << x << ' '; }
-template<typename T> void print2d(const vector<vector<T>>& v) { for (auto &row : v) { for (auto &x : row) cout << x << ' '; cout << '\n'; } }
-ll t=1,n,m,p,q,r,k,c,x,y,z;
-const ll INF = 1e18, MOD = 1e9+7;
-
-void solve() {
-    cin>>n;
-    vector<ll> a(n);
-    read(a);
-    unordered_map<ll,ll> mpp;
-    for(ll i: a){
-        mpp[i]++;
-    }
-    unordered_map<ll,ll> mpp2;
-    for(auto it: mpp){
-        mpp2[it.second] += it.first;
-    }
-    priority_queue<pll> pq;
-    for(auto it: mpp2){
-        pq.push(it);
-    }
-    ll sa = 0, sb = 0;
-    int turn = 1;
-    while(!pq.empty()){
-        auto [f, e] = pq.top();
-        pq.pop();
-        if(e%2==0){
-            sa += f*(e/2);
-            sb += f*(e/2);
-        }else{
-            sa += f*(e/2 + turn);
-            sb += f*(e/2 + !turn);
-            turn = !turn;
-        }
-    }
-    cout<<sa<<" "<<sb<<"\n";
-}
-
-int main() {
-    fastio();
+#define int long long 
+ 
+int32_t main()
+{
+    int t;
     cin >> t;
-    while (t--) solve();
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<long long> arr;
+        map<long long, long long> mp;
+        vector<pair<long long, long long>> mp2;
+        for (int i = 0; i < n; i++)
+        {
+            long long a;
+            cin >> a;
+            mp[a]++;
+            arr.push_back(a);
+        }
+        for (auto &it : mp)
+        {
+            mp2.push_back({it.second, it.first});
+        }
+        std::sort(mp2.begin(), mp2.end(), [](const auto& a, const auto& b) {
+        return a.first > b.first;
+    });
+        bool b = 0;
+        int ann = 0, bob = 0;
+        for (auto &it : mp2)
+        {
+            if (it.second % 2 == 1)
+            {
+                if (b == 0)
+                {
+ 
+                    ann += (it.first * ((it.second / 2) + 1));
+                    bob += (it.first * (it.second / 2));
+                    b = 1;
+                    // cout<<ann<<" "<<bob<<endl;
+                }
+                else
+                {
+                    bob += (it.first * ((it.second / 2) + 1));
+                    ann += (it.first * (it.second / 2));
+                    b = 0;
+                }
+            }
+            else
+            {
+                ann += (it.first * ((it.second / 2)));
+                bob += (it.first * (it.second / 2));
+            }
+        }
+        cout << ann << " " << bob << endl;
+    }
     return 0;
 }
