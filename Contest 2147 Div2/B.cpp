a6f4e0db02daae342d0a80848d459e829deb1ef0
shivanshcoding <shivanshcoding2005@gmail.com>
2025-10-05 14:36:03 +0530

    Grind for 5 Oct 2025

--- Contest 2147 Div2/B.cpp
+++ Contest 2147 Div2/B.cpp
@@ -1,74 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-struct custom_hash {
-   static uint64_t splitmix64(uint64_t x) {
-       x += 0x9e3779b97f4a7c15;
-       x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-       x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-       return x ^ (x >> 31);
-   }
-size_t operator()(uint64_t x) const {
-       static const uint64_t FIXED_RANDOM =
-       chrono::steady_clock::now().time_since_epoch().count();
-       return splitmix64(x + FIXED_RANDOM);
-   }
-};
-#define prDouble(x) cout<<fixed<<setprecision(10)<<x
-#define fastio() ios::sync_with_stdio(false); cin.tie(nullptr)
-#define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
-#define pb push_back
-#define f first
-#define s second
-#define sz(x) (int)(x).size()
-using ll = long long;
-using ld = long double;
-using pll = pair<ll,ll>;
-using tll = tuple<ll,ll,ll>;
-using vll = vector<ll>;
-using vpll = vector<pll>;
-vector<ll> dx = {1, -1, 0, 0}, dy = {0, 0, 1, -1}; // for grid
-vector<ll> ddx = {1,1,0,-1,-1,-1,0,1}, ddy = {0,1,1,1,0,-1,-1,-1}; // 8 directions
-template<typename T> void read(vector<T> &v) { for (auto &x : v) cin >> x; }
-template<typename T> void printv(const vector<T>& v) { for (auto &x : v) cout << x << ' '; }
-template<typename T> void print2d(const vector<vector<T>>& v) { for (auto &row : v) { for (auto &x : row) cout << x << ' '; cout << '\n'; } }
-ll t=1,n,m,p,q,r,k,a,b,c,x,y,z;
-const ll INF = 1e18, MOD = 1e9+7;
-
-void solve() {
+ 
+int t, n;
+ 
+signed main() {
+  ios::sync_with_stdio(false); cin.tie(nullptr);
+ 
+  cin >> t;
+  while (t--) {
     cin >> n;
-    int size = 2 * n;
-
-    vector<int> result(size + 1, 0);
-
-    // Use a set to store free positions
-    set<int> freePositions;
-    for (int i = 1; i <= size; ++i) freePositions.insert(i);
-
-    for (int val = n; val >= 1; --val) {
-        // Pick the smallest available position
-        auto it = freePositions.begin();
-        int first = *it;
-        freePositions.erase(it);
-
-        // Find the second position as first + val or next available
-        int second = first + val;
-        while (freePositions.find(second) == freePositions.end()) second += val;
-        freePositions.erase(second);
-
-        result[first] = result[second] = val;
+    for (int i = n; i >= 1; i--) {
+      cout << i << " ";
     }
-
-    for (int i = 1; i <= size; ++i) {
-        if (i > 1) cout << ' ';
-        cout << result[i];
+    cout << n;
+    for (int i = 1; i < n; i++) {
+      cout << " " << i;
     }
-    cout << '\n';
-}
-
-int main() {
-    fastio();
-    cin >> t;
-    while (t--) solve();
-    return 0;
+    cout << "\n";
+  }
 }
--- Contest 2147 Div2/C. Rabbits.cpp
+++ Contest 2147 Div2/C. Rabbits.cpp
@@ -1,70 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-// bool isZeroSegmentPossible(int firstPos, const vector<bool>& canLeft, const vector<bool>& canRight) {
-//     // Returns bitmask of possible placements for a single zero
-//     int cur = (canLeft[firstPos] ? 1 : 0) | (canRight[firstPos] ? 2 : 0);
-//     return cur;
-// }
-
-bool validateSegment(const deque<int>& segment, const vector<bool>& canLeft, const vector<bool>& canRight) {
-    // Validate a contiguous segment of zeros with ones in between
-    int cur = (canLeft[segment.front()] ? 1 : 0) | (canRight[segment.front()] ? 2 : 0);
-    if (!cur) return false;
-
-    for (size_t i = 1; i < segment.size(); ++i) {
-        int pos = segment[i];
-        int next = 0;
-        if ((cur & 1) && canRight[pos]) next |= 2;
-        if ((cur & 2) && canLeft[pos]) next |= 1;
-        cur = next;
-        if (!cur) return false;
-    }
-    return true;
+ 
+void solve() {
+	int n; cin >> n;
+	string s; cin >> s;
+	bool ok = true;
+	bool curr = (s[0] == '1');
+	int cnt = 0;
+	for (int i = 0; i < n; i++) {
+		if (s[i] == '0')
+			cnt++;
+		if (i == 0)
+			continue;
+		if (s[i] == s[i-1] && s[i] == '0')
+			curr = false;
+		if (s[i] == s[i-1] && s[i] == '1') {
+			if (curr && cnt % 2 == 1)
+				ok = false;
+			curr = true;
+			cnt = 0;
+		}
+	}
+	
+	if (curr && cnt % 2 == 1 && s[n-1] == '1')
+		ok = false;
+		
+	cout << (ok ? "YES" : "NO") << "\n";
 }
-
-bool cp_solution(const string& s) {
-    int n = s.size();
-    deque<int> zeros;
-    for (int i = 0; i < n; ++i) if (s[i] == '0') zeros.push_back(i);
-    if (zeros.empty()) return true;
-
-    vector<bool> canLeft(n, false), canRight(n, false);
-    for (int i : zeros) {
-        canLeft[i]  = (i == 0) || (s[i-1] == '0') || (i >= 2 && s[i-1] == '1' && s[i-2] == '0');
-        canRight[i] = (i == n-1) || (s[i+1] == '0') || (i + 2 < n && s[i+1] == '1' && s[i+2] == '0');
-    }
-
-    size_t idx = 0;
-    while (idx < zeros.size()) {
-        deque<int> segment;
-        segment.push_back(zeros[idx]);
-        size_t j = idx;
-        while (j + 1 < zeros.size() && zeros[j+1] == zeros[j] + 2 && s[zeros[j] + 1] == '1') {
-            segment.push_back(zeros[j+1]);
-            ++j;
-        }
-
-        if (!validateSegment(segment,canLeft, canRight)) return false;
-        idx = j + 1;
-    }
-    return true;
-}
-
-void processTestCases() {
-    int T;
-    if (!(cin >> T)) return;
-    while (T--) {
-        int n;
-        string s;
-        cin >> n >> s;
-        cout << (cp_solution(s) ? "YES" : "NO") << '\n';
-    }
-}
-
+ 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    processTestCases();
-    return 0;
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+	int T; cin >> T;
+	while (T--)
+		solve();
 }
