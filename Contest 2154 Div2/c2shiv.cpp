c68bdfaef2f88a09de18f9fa67b5eb3bbf2a92c6
shivanshcoding <shivanshcoding2005@gmail.com>
2025-10-26 00:26:37 +0530

    Grind for 26 Oct 2025

--- Contest 2154 Div2/c2shiv.cpp
+++ Contest 2154 Div2/c2shiv.cpp
@@ -1,57 +1,86 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-struct custom_hash {
-   static uint64_t splitmix64(uint64_t x) {
-       x += 0x9e3779b97f4a7c15;
-       x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-       x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-       return x ^ (x >> 31);
-   }
-size_t operator()(uint64_t x) const {
-       static const uint64_t FIXED_RANDOM =
-       chrono::steady_clock::now().time_since_epoch().count();
-       return splitmix64(x + FIXED_RANDOM);
-   }
-};
-#define prDouble(x) cout<<fixed<<setprecision(10)<<x
-#define fastio() ios::sync_with_stdio(false); cin.tie(nullptr)
-#define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
-#define pb push_back
-#define f first
-#define sz(x) (int)(x).size()
-using ll = long long;
-using ld = long double;
-using pll = pair<ll,ll>;
-using tll = tuple<ll,ll,ll>;
-using vll = vector<ll>;
-using vpll = vector<pll>;
-vector<ll> dx = {1, -1, 0, 0}, dy = {0, 0, 1, -1}; // for grid
-vector<ll> ddx = {1,1,0,-1,-1,-1,0,1}, ddy = {0,1,1,1,0,-1,-1,-1}; // 8 directions
-template<typename T> void read(vector<T> &v) { for (auto &x : v) cin >> x; }
-template<typename T> void printv(const vector<T>& v) { for (auto &x : v) cout << x << ' '; }
-template<typename T> void print2d(const vector<vector<T>>& v) { for (auto &row : v) { for (auto &x : row) cout << x << ' '; cout << '\n'; } }
-ll t=1,n,m,p,q,r,k,a,b,c,x,y,z;
-const ll INF = 1e18, MOD = 1e9+7;
+typedef long long ll;
+#define debug(x) cout << #x << " = " << x << "\n";
+#define vdebug(a) cout << #a << " = "; for(auto x: a) cout << x << " "; cout << "\n";
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int uid(int a, int b) { return uniform_int_distribution<int>(a, b)(rng); }
+ll uld(ll a, ll b) { return uniform_int_distribution<ll>(a, b)(rng); }
 
-void solve() {
-    cin>>n;
-    vector<ll> a(n),b(n);
-    read(a);
-    read(b);
-    unordered_set<pll> s;
-    ll ans = 0;
-    for(int i=0;i<n;i++){
-        ll curr = a[i];
-        for(int j=2;j*j<=curr;)
+const int N = 2e5 + 10;
+vector<vector<int>> pfac(N + 1);
+
+void solve(){
+    int n;
+    cin >> n;
+
+    vector<int> a(n), b(n);
+    for (int &x : a) cin >> x;
+    for (int &x : b) cin >> x;
+
+    vector<int> ord(n);
+    iota(ord.begin(), ord.end(), 0);
+    sort(ord.begin(), ord.end(), [&](int x, int y) -> bool{
+        return b[x] < b[y];
+    });
+
+    int ans = b[ord[0]] + b[ord[1]];
+    map<int, int> cnt;
+    for (int i = 0; i < n; i++){
+        for (int x : pfac[a[i]]){
+            if (cnt[x] > 0)
+                ans = 0;
+            cnt[x]++;
+        }
     }
 
+    for (int i = 0; i < n; i++){
+        for (int x : pfac[a[i]])
+            cnt[x]--;
 
+        for (int x : pfac[a[i] + 1]){
+            if (cnt[x] > 0)
+                ans= min(ans, b[i]);
+        }
+
+        for (int x : pfac[a[i]])
+            cnt[x]++;
+    }
+
+    int idx = ord[0];
+    vector<int> check;
+    for (int i = 0; i < n; i++){
+        if (i == idx)
+            continue;
+
+        for (int x : pfac[a[i]])
+            check.push_back(x);
+    }
+
+    for (int x : check){
+        int times = x - (a[idx] % x);
+        if (times == x)
+            times = 0;
+
+        ans = min(1LL * ans, 1LL * times * b[idx]);
+    }
+
+    cout << ans << "\n";
 }
 
-int main() {
-    fastio();
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    for (int i = 2; i <= N; i++){
+        if (!pfac[i].empty())
+            continue;
+
+        for (int j = i; j <= N; j += i)
+            pfac[j].push_back(i);
+    }
+    
+    int t;
     cin >> t;
     while (t--) solve();
-    return 0;
 }
--- Contest 2154 Div2/d.cpp
+++ Contest 2154 Div2/d.cpp
@@ -1,161 +1,85 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-struct custom_hash {
-   static uint64_t splitmix64(uint64_t x) {
-       x += 0x9e3779b97f4a7c15;
-       x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-       x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-       return x ^ (x >> 31);
-   }
-size_t operator()(uint64_t x) const {
-       static const uint64_t FIXED_RANDOM =
-       chrono::steady_clock::now().time_since_epoch().count();
-       return splitmix64(x + FIXED_RANDOM);
-   }
-};
-#define prDouble(x) cout<<fixed<<setprecision(10)<<x
-#define fastio() ios::sync_with_stdio(false); cin.tie(nullptr)
-#define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
-#define pb push_back
-#define f first
-#define s second
-#define sz(x) (int)(x).size()
-using ll = long long;
-using ld = long double;
-using pll = pair<ll,ll>;
-using tll = tuple<ll,ll,ll>;
-using vll = vector<ll>;
-using vpll = vector<pll>;
-vector<ll> dx = {1, -1, 0, 0}, dy = {0, 0, 1, -1}; // for grid
-vector<ll> ddx = {1,1,0,-1,-1,-1,0,1}, ddy = {0,1,1,1,0,-1,-1,-1}; // 8 directions
-template<typename T> void read(vector<T> &v) { for (auto &x : v) cin >> x; }
-template<typename T> void printv(const vector<T>& v) { for (auto &x : v) cout << x << ' '; }
-template<typename T> void print2d(const vector<vector<T>>& v) { for (auto &row : v) { for (auto &x : row) cout << x << ' '; cout << '\n'; } }
-ll t=1,n,m,p,q,r,k,a,b,c,x,y,z;
-const ll INF = 1e18, MOD = 1e9+7;
+typedef long long ll;
+#define debug(x) cout << #x << " = " << x << "\n";
+#define vdebug(a) cout << #a << " = "; for(auto x: a) cout << x << " "; cout << "\n";
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int uid(int a, int b) { return uniform_int_distribution<int>(a, b)(rng); }
+ll uld(ll a, ll b) { return uniform_int_distribution<ll>(a, b)(rng); }
 
-class TreeNavigator {
-private:
+void solve(){
     int n;
-    vector<vector<int>> graph;
-    vector<int> parent;
-    vector<bool> path_marker;
-    vector<int> main_path;
-    vector<pair<int, int>> operations;
+    cin >> n;
 
-    bool dfsPathFinder(int node, int parent_node, int destination) {
-        parent[node] = parent_node;
-        if (node == destination) return true;
-        for (int neighbor : graph[node]) {
-            if (neighbor != parent_node) {
-                if (dfsPathFinder(neighbor, node, destination))
-                    return true;
-            }
+    vector<vector<int>> g(n);
+    for (int i = 0; i < n - 1; i++){
+        int u, v;
+        cin >> u >> v;
+        u--;
+        v--;
+
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+
+    vector<int> dep(n), par(n), ch(n);
+    auto dfs = [&](auto self, int c, int p) -> void{
+        if (p != -1)
+            dep[c] = dep[p] + 1;
+        par[c] = p;
+        
+        for (int x : g[c]){
+            if (x == p) continue;
+            
+            self(self, x, c);
+            ch[c]++;
         }
-        return false;
+    };
+    dfs(dfs, n - 1, -1);
+
+    array<vector<int>, 2> leaves;
+    leaves[0];
+
+    for (int i = 0; i < n; i++){
+        if (ch[i] == 0)
+            leaves[dep[i] & 1].push_back(i);
     }
 
-    void buildMainPath() {
-        main_path.clear();
-        int current = n;
-        while (current != 0) {
-            main_path.push_back(current);
-            path_marker[current] = true;
-            current = parent[current];
+    vector<array<int, 2>> ans;
+    int col = dep[0] & 1;
+    for (int i = 0; i < n - 1; i++){
+        if (leaves[col ^ 1].empty()){
+            ans.push_back({1, -1});
+            col ^= 1;
         }
-        reverse(main_path.begin(), main_path.end());
+
+        int nxt = leaves[col ^ 1].back();
+        leaves[col ^ 1].pop_back();
+        ans.push_back({2, nxt});
+
+        int p = par[nxt];
+        ch[p]--;
+        if (ch[p] == 0)
+            leaves[dep[p] & 1].push_back(p);
+
+        ans.push_back({1, -1});
+        col ^= 1;
     }
 
-    void pruneAndRecord() {
-        vector<set<int>> tempGraph(n + 1);
-        for (int i = 1; i <= n; ++i)
-            for (int v : graph[i])
-                tempGraph[i].insert(v);
-
-        operations.clear();
-        for (size_t i = 0; i + 1 < main_path.size(); ++i) {
-            int u = main_path[i];
-            int next = main_path[i + 1];
-
-            vector<int> neighbors(tempGraph[u].begin(), tempGraph[u].end());
-
-            for (int neighbor : neighbors) {
-                if (neighbor == next) continue;
-
-                // stabilize movement
-                if (i > 0 || !operations.empty()) {
-                    operations.emplace_back(1, 0);
-                    operations.emplace_back(1, 0);
-                }
-
-                operations.emplace_back(2, neighbor);
-                tempGraph[u].erase(neighbor);
-                tempGraph[neighbor].erase(u);
-            }
-
-            operations.emplace_back(1, 0);
-        }
+    cout << ans.size() << "\n";
+    for (auto [x, y] : ans){
+        if (x == 1)
+            cout << "1\n";
+        else
+            cout << "2 " << y + 1 << "\n";
     }
+}
 
-public:
-    TreeNavigator(int nodes) {
-        reset(nodes);
-    }
-
-    void reset(int nodes) {
-        n = nodes;
-        graph.assign(n + 1, {});
-        parent.assign(n + 1, 0);
-        path_marker.assign(n + 1, false);
-        main_path.clear();
-        operations.clear();
-    }
-
-    void addEdge(int u, int v) {
-        graph[u].push_back(v);
-        graph[v].push_back(u);
-    }
-
-    void process() {
-        dfsPathFinder(1, 0, n);
-        buildMainPath();
-        pruneAndRecord();
-    }
-
-    void printResults() const {
-        cout << operations.size() << "\n";
-        for (auto [type, val] : operations) {
-            if (type == 1) cout << "1\n";
-            else cout << "2 " << val << "\n";
-        }
-    }
-};
-
-class Solution {
-public:
-    void run() {
-        ios::sync_with_stdio(false);
-        cin.tie(nullptr);
-
-        cin >> t;
-        while (t--) {
-            cin >> n;
-            TreeNavigator solver(n);
-
-            for (int i = 0; i < n - 1; ++i) {
-                int u, v;
-                cin >> u >> v;
-                solver.addEdge(u, v);
-            }
-
-            solver.process();
-            solver.printResults();
-        }
-    }
-};
-
-int main() {
-    Solution().run();
-    return 0;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int t;
+    cin >> t;
+    while (t--) solve();
 }
