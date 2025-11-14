2bc082b8e8982f506e0c6ed8a7f0399dd2f88750
shivanshcoding <shivanshcoding2005@gmail.com>
2025-11-14 20:41:58 +0530

    Contest Grind ABC Done

---
+++ Contest 2169 Div2/c.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct custom_hash {
+   static uint64_t splitmix64(uint64_t x) {
+       x += 0x9e3779b97f4a7c15;
+       x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+       x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+       return x ^ (x >> 31);
+   }
+size_t operator()(uint64_t x) const {
+       static const uint64_t FIXED_RANDOM =
+       chrono::steady_clock::now().time_since_epoch().count();
+       return splitmix64(x + FIXED_RANDOM);
+   }
+};
+#define prDouble(x) cout<<fixed<<setprecision(10)<<x
+#define fastio() ios::sync_with_stdio(false); cin.tie(nullptr)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define f first
+#define s second
+#define sz(x) (int)(x).size()
+using ll = long long;
+using ld = long double;
+using pll = pair<ll,ll>;
+using tll = tuple<ll,ll,ll>;
+using vll = vector<ll>;
+using vpll = vector<pll>;
+vector<ll> dx = {1, -1, 0, 0}, dy = {0, 0, 1, -1}; // for grid
+vector<ll> ddx = {1,1,0,-1,-1,-1,0,1}, ddy = {0,1,1,1,0,-1,-1,-1}; // 8 directions
+template<typename T> void read(vector<T> &v) { for (auto &x : v) cin >> x; }
+template<typename T> void printv(const vector<T>& v) { for (auto &x : v) cout << x << ' '; }
+template<typename T> void print2d(const vector<vector<T>>& v) { for (auto &row : v) { for (auto &x : row) cout << x << ' '; cout << '\n'; } }
+ll t=1,n,m,p,q,r,k,a,b,c,x,y,z;
+const ll INF = 1e18, MOD = 1e9+7;
+
+
+
+void solve() {
+    cin >> n;
+    vector<ll> pref(n + 1);
+    pref[0] = 0;
+
+    for (int i = 1; i <= n; i++) {
+        cin >> x;
+        pref[i] = pref[i - 1] + x;
+    }
+    ll og = pref[n];
+
+    ll mg = 0, mh = LLONG_MIN; 
+
+    for (ll r = 1; r <= n; r++) {
+        ll currh = -(r * r) + r + pref[r - 1];
+        mh = max(mh, currh);
+        
+        ll currg = (r * r) + r - pref[r] + mh;
+        mg = max(mg, currg);
+    }
+
+    cout << og + mg << "\n";
+}
+
+
+int main() {
+    fastio();
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
