7ed7a207fd5b1c5e45df5cfe058dcea323b8b545
shivanshcoding <shivanshcoding2005@gmail.com>
2025-12-05 21:09:52 +0530

    C done

---
+++ Contest 2173 Div2/C.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct custom_hash {
+   static uint64_t splitmix64(uint64_t x) {
+       x += 0x9e3779b97f4a7c15;
+       x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+       x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+       return x ^ (x >> 31);
+   }
+size_t operator()(uint64_t x) const {
+       static const uint64_t FIXED_RANDOM =
+       chrono::steady_clock::now().time_since_epoch().count();
+       return splitmix64(x + FIXED_RANDOM);
+   }
+};
+#define prDouble(x) cout<<fixed<<setprecision(10)<<x
+#define fastio() ios::sync_with_stdio(false); cin.tie(nullptr)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define f first
+#define s second
+#define sz(x) (int)(x).size()
+using ll = long long;
+using ld = long double;
+using pll = pair<ll,ll>;
+using tll = tuple<ll,ll,ll>;
+using vll = vector<ll>;
+using vpll = vector<pll>;
+vector<ll> dx = {1, -1, 0, 0}, dy = {0, 0, 1, -1}; // for grid
+vector<ll> ddx = {1,1,0,-1,-1,-1,0,1}, ddy = {0,1,1,1,0,-1,-1,-1}; // 8 directions
+template<typename T> void read(vector<T> &v) { for (auto &x : v) cin >> x; }
+template<typename T> void printv(const vector<T>& v) { for (auto &x : v) cout << x << ' '; }
+template<typename T> void print2d(const vector<vector<T>>& v) { for (auto &row : v) { for (auto &x : row) cout << x << ' '; cout << '\n'; } }
+ll t=1,n,m,p,q,r,k,a,b,c,x,y,z;
+const ll INF = 1e18, MOD = 1e9+7;
+
+void solve() {
+    cin>>n>>k;
+    vector<ll> a(n);
+    read(a);
+    if(n==k){
+        cout<<"1\n1\n";
+        return;
+    }
+    sort(all(a));
+    a.erase(unique(all(a)), a.end());
+    unordered_map<ll,ll> mpp;
+    for(ll& x: a){
+        mpp[x] = 0;
+    }
+    unordered_set<ll> ans;
+    for(ll& x: a){
+        if(mpp[x]==1){
+            continue;
+        }
+        if (k / x > n) {
+            cout << -1 << "\n";
+            return;
+        }
+        for(int j=x;j<=k;j+=x){
+            if(mpp.find(j)==mpp.end()){
+                cout<<-1<<"\n";
+                return;
+            }else{
+                mpp[j] = 1;
+            }
+        }
+        ans.insert(x);
+    }
+    cout<<ans.size()<<"\n";
+    for(ll c: ans){
+        cout<<c<<" ";
+    }
+    cout<<"\n";
+}
+
+int main() {
+    fastio();
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
